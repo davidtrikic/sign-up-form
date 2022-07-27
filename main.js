acb8f3a51aaad7710a89ca9b31dc5c449ab3b24a
David <41009202+davidtrikic@users.noreply.github.com>
2022-07-27 17:12:39 +0200

    add password validation

---
+++ main.js
@@ -0,0 +1,28 @@
+const signupForm = document.querySelector('.main-form');
+const passwordDiv = document.querySelector('#password-confirm').parentElement;
+const passwordInput = document.querySelector('#password');
+const passwordConfirmInput = document.querySelector('#password-confirm');
+
+const validatePassword = () => {
+    const password = passwordInput.value;
+    const passwordConfirm = passwordConfirmInput.value;
+    console.log(password);
+    
+    console.log(passwordConfirm);
+
+    if(password != passwordConfirm) {
+        
+        passwordDiv.classList.add('error-text');
+        passwordInput.classList.add('error');
+        passwordConfirmInput.classList.add('error');
+    }
+    if (password === passwordConfirm) {
+        passwordDiv.classList.remove('error-text');
+        passwordInput.classList.remove('error');
+        passwordConfirmInput.classList.remove('error');
+    }
+    
+}
+
+signupForm.addEventListener('submit', function(e) {e.preventDefault();});
+passwordConfirmInput.addEventListener('input', validatePassword);
